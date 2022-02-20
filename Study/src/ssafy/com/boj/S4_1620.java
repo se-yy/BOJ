b3eaacdb0d8501ecc22edc9209128178706206cf
Seyoung Shin <pipixpfjs@gmail.com>
2022-02-20 19:01:13 +0900

    1620 update

--- Study/src/ssafy/com/boj/S4_1620.java
+++ Study/src/ssafy/com/boj/S4_1620.java
@@ -5,6 +5,8 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class S4_1620 {
@@ -17,25 +19,25 @@ public class S4_1620 {
 		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
+		Map<String, Integer> map = new HashMap<>();
+		String[] name = new String[N+1];
+		String q, s;
 		
-		String[] pkm = new String[N+1];
-		for (int i = 1; i <= N; i++)
-			pkm[i] = br.readLine();
+		for (int i = 1; i <= N; i++) {
+			s = br.readLine();
+			map.put(s, i);
+			name[i] = s;
+		}
 		
-		String q;
 		for (int i = 0; i < M; i++) {
 			q = br.readLine();
-			if (isNum(q)) { // 숫자일때
-				int n = Integer.parseInt(q);
-				sb.append(pkm[n]).append("\n");
-			} else { // 문자일때
-				for (int j = 1; j <= N; j++) {
-					if (q.equals(pkm[j])) {
-						sb.append(j).append("\n");
-						break;
-					}
-				}
+			if (isNum(q)) { // 숫자일 때
+				sb.append(name[Integer.parseInt(q)]);
 			}
+			else { // 문자일때
+				sb.append(map.get(q));
+			}
+			sb.append("\n");
 		}
 		
 		bw.write(sb.toString());
@@ -44,12 +46,11 @@ public class S4_1620 {
 		br.close();
 	}
 	static boolean isNum(String s) {
-		boolean res = false;
-		
-		char c = s.charAt(0);
-		if (c>='1' && c<='9')
-			res = true;
-		
-		return res;
+		try {
+			Integer.parseInt(s);
+			return true;
+		}catch(NumberFormatException e) {
+			return false;
+		}
 	}
 }
