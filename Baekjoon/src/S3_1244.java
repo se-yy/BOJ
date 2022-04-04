54155bec693115f04fbb3c74630690466ae2b188
Seyoung Shin <pipixpfjs@gmail.com>
2022-04-04 22:30:11 +0900

    1244번 스위치 켜고 끄기

--- Baekjoon/src/S3_1244.java
+++ Baekjoon/src/S3_1244.java
@@ -1,56 +1,69 @@
-
-
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class S3_1244 {
-	static boolean[] arr;
-	static int N;
 	
-	public static void boy(int n) {
-		for (int i = 0; i < N; i++) {
-			if ((i+1)%n == 0)
-				arr[i] = !(arr[i]);
-		}
-	}
-	public static void girl(int n) {
-		arr[n] = !(arr[n]);
-			
-		for (int i = 1; i <= N/2; i++) {
-			if (n-i >= 0 && n+i < N && arr[n-i] == arr[n+i]) {
-				arr[n-i] = !(arr[n-i]);
-				arr[n+i] = !(arr[n+i]);
-			} else break;
-		}
-	}
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	static int[] arr;
+	
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		arr = new boolean[N];
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			if (Integer.parseInt(st.nextToken())==1)
-				arr[i] = true;
-			else arr[i] = false;
-		}
+		int n = Integer.parseInt(br.readLine());
+		arr = new int[n+1];
 		
-		int t = Integer.parseInt(br.readLine());
-		for (int i = 0; i < t; i++) {
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= n; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		
+		int cnt = Integer.parseInt(br.readLine());
+		for (int i = 0; i < cnt; i++) {
 			st = new StringTokenizer(br.readLine());
-			if (Integer.parseInt(st.nextToken())==1)
-				boy(Integer.parseInt(st.nextToken()));
-			else
-				girl(Integer.parseInt(st.nextToken())-1);
+			int m = Integer.parseInt(st.nextToken());
+			int p = Integer.parseInt(st.nextToken());
+			
+			switch(m) {
+			case 1:
+				for (int j = 1; j <= n/p; j++) {
+					int temp = p*j;
+					change(temp);
+				}
+				break;
+			case 2:
+				change(p);
+				for (int j = 1; j <= n/2; j++) {
+					if (p-j < 1 || p+j > n)
+						break;
+					
+					if (arr[p-j] != arr[p+j])
+						break;
+					
+					change(p-j); change(p+j);
+				}
+				break;
+			}
+			
 		}
-		for (int i = 0; i < N; i++) {
-			if (arr[i])
-				System.out.print(1);
-			else System.out.print(0);
-			System.out.printf((i+1)%20==0?"%n":" ");
-		}
+		
+		for (int i = 1; i <= n; i++)
+			sb.append(arr[i]).append(" ");
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
 	}
-
+	
+	static void change(int t) {
+		if (arr[t]==0)
+			arr[t] = 1;
+		else
+			arr[t] = 0;
+	}
 }
